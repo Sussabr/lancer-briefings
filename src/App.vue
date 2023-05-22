b9a58a2d829907f32269ce47e013534d3e285ef4
Sussabr <134245813+Sussabr@users.noreply.github.com>
2023-05-22 09:58:08 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -96,7 +96,7 @@ export default {
         {
           "slug": "001",
           "name": "Bug-Hunt",
-          "status": "success"
+          "status": "start"
         },
       ],
       "pilots": [
