da7e8a44f54e3e1249253e2c5bd2564b56b0db84
Sussabr <134245813+Sussabr@users.noreply.github.com>
2023-05-22 09:51:33 -0300

    Update Mission.vue

--- src/components/Mission.vue
+++ src/components/Mission.vue
@@ -30,7 +30,7 @@ export default {
 			return `/icons/mission-${this.mission.status}.svg`
 		},
 		missionStatus() {
-			if (this.mission.status === "start") return "Current\nBriefing"
+			if (this.mission.status === "start") return "Missão em\nAndamento"
 			if (this.mission.status === "partial-success") return "Partial\nSuccess"
 			if (this.mission.status === "success") return "Mission\nSuccess"
 			if (this.mission.status === "failure") return "Mission\nFailure"
