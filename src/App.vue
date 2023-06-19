6f6264737a28d82dd8f910b13999b00dfb8d966a
Sussabr <134245813+Sussabr@users.noreply.github.com>
2023-06-19 03:04:46 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <Header :header="this.header" />
+  <v-container>
   <div class="content-container">
+   <v-row>
+      <v-col cols="12" md="6">
     <section class="section-container" id="missions" style="width:435px; height:714px;">
       <div class="section-header clipped-medium-backward">
         <img src="/icons/mission-icon.svg" />
@@ -20,6 +23,8 @@
           />
         </div>
       </div>
+        </v-col>
+      <v-col cols="12" md="6">
     </section>
     <section class="section-container" id="events" style="width:435px; height:714px;">
       <div class="section-header clipped-medium-backward">
@@ -29,6 +34,10 @@
       <div class="section-content-container">
         <Markdown :source="events" class="markdown" />
       </div>
+       </v-col>
+    </v-row>
+    <v-row>
+     <v-col cols="12">
     </section>
     <section class="section-container" id="pilots" style="
     padding:0;
@@ -47,6 +56,8 @@
         </div>
       </div>
     </section>
+      </v-col>
+    </v-row>
   </div>
   <svg
     style="visibility: hidden; position: relative;"
@@ -71,6 +82,7 @@
   <audio autoplay>
     <source src="/startup.ogg" type="audio/ogg" />
   </audio>
+  </v-container>
   <Footer/>
 </template>
 <script>
@@ -179,16 +191,200 @@ export default {
 
 <style lang="scss">
 #app {
-  width: 1902px;
-  height: 910px;
+ <template>
+  <Header :header="this.header" />
+  <div class="content-container">
+    <section class="section-container" id="missions" style="width:435px; height:714px;">
+      <div class="section-header clipped-medium-backward">
+        <img src="/icons/mission-icon.svg" />
+        <h1>Missões</h1>
+      </div>
+      <div class="section-content-container">
+        <h3>Objetivo Atual</h3>
+        <Markdown :source="current_md" class="markdown" />
+        <h3>Lista de Missões</h3>
+        <div class="mission-list-container">
+          <Mission
+            v-for="item in this.missions"
+            :key="item.slug"
+            :mission="item"
+            :selected="this.mission_slug"
+            @click="selectMission(item)"
+          />
+        </div>
+      </div>
+    </section>
+    <section class="section-container" id="events" style="width:435px; height:714px;">
+      <div class="section-header clipped-medium-backward">
+        <img src="/icons/events-icon.svg" />
+        <h1>Eventos da missão</h1>
+      </div>
+      <div class="section-content-container">
+        <Markdown :source="events" class="markdown" />
+      </div>
+    </section>
+    <section class="section-container" id="pilots" style="width:894px; height:714px;">
+      <div style="height:52px; overflow:hidden;">
+        <div class="section-header clipped-medium-backward-pilot">
+          <img src="/icons/pilot-icon.svg" />
+          <h1>Pilotos</h1>
+        </div>
+        <div class="rhombus-back">&nbsp;</div>
+      </div>
+      <div class="section-content-container">
+        <div class="pilot-list-container">
+          <Pilot v-for="item in this.pilots" :key="item.slug" :pilot="item" />
+        </div>
+      </div>
+    </section>
+  </div>
+  <svg
+    style="visibility: hidden; position: absolute;"
+    width="0"
+    height="0"
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.1"
+  >
+    <defs>
+      <filter id="round">
+        <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
+        <feColorMatrix
+          in="blur"
+          mode="matrix"
+          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -5"
+          result="goo"
+        />
+        <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+      </filter>
+    </defs>
+  </svg>
+  <audio autoplay>
+    <source src="/public/startup.ogg" type="audio/ogg" />
+  </audio>
+  <Footer/>
+</template>
+
+<script>
+import Header from './components/layout/Header.vue';
+import Footer from './components/layout/Footer.vue';
+import Mission from './components/Mission.vue';
+import Pilot from './components/Pilot.vue';
+import Markdown from 'vue3-markdown-it';
+
+export default {
+  components: {
+    Header,
+    Footer,
+    Mission,
+    Pilot,
+    Markdown
+  },
+
+  data() {
+    return {
+      "mission_slug": "001",
+      "current_md": "",
+      "events": "",
+      "missions": [
+        {
+  "slug": "001",
+          "name": "TBA",
+          "status": "start"
+        }, 
+      ],
+      "pilots": [
+        {
+         "callsign": "Titano",
+          "alias": "Zahid Raja",
+          "code": "ede41c32-7cf2424f-99d8-d249da109a26//NDL-C-BRASS-WILD",
+          "corpro": "GMS",
+          "frame": "Everest",
+          "mech": "Allos"
+        },
+          {
+         "callsign": "Ordis",
+          "alias": "Ordan Karris",
+          "code": "91320fec-2acd-4062-bb34-c8e11f6eb851//NDL-C-EARNEST-HUB",
+          "corpro": "SSC",
+          "frame": "Atlas",
+          "mech": "Umbra"
+        },
+      ],
+      "header": {
+        "planet": "Hercynia",
+        "year": "5014u",
+        "system": "Ardennes-3",
+        "gate": "Atlas-Quanoukrim",
+        "ring": "Atlas",
+        "headerTitle": "DEPARTAMENTO NAVAL DA UNIÃO",
+        "headerSubtitle": " ",
+        "subheaderTitle": "Time de resposta imediata",
+        "subheaderSubtitle": "[sem nome]",
+      },
+      "options":{
+        "eventsMarkdownPerMission": true
+      }
+    }
+  },
+
+  created() {
+    this.loadMissionMarkdown()
+    this.loadEventsMarkdown()
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+    selectMission(mission) {
+      this.mission_slug = mission.slug;
+      this.loadMissionMarkdown()
+      if(this.options.eventsMarkdownPerMission){
+        this.loadEventsMarkdown();
+      }
+    },
+    loadMissionMarkdown() {
+      let self = this;
+      let md = `/missions/${self.mission_slug}.md`
+      var client = new XMLHttpRequest();
+      client.open('GET', md);
+      client.onreadystatechange = function () {
+        self.current_md = client.responseText;
+      }
+      client.send();
+    },
+    loadEventsMarkdown() {
+      let self = this;
+      let md = "";
+
+      if(self.options.eventsMarkdownPerMission){
+        md = `/events/${self.mission_slug}.md`
+      }
+      else {
+        md = "/events.md"
+      }
+
+      var client = new XMLHttpRequest();
+      client.open('GET', md);
+      client.onreadystatechange = function () {
+        self.events = client.responseText;
+      }
+      client.send();
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
   overflow: column;
 }
 
 @media (max-width: 767px) {
-#app {
- width: 390px;
-  height: 860px;
-  overflow: column;
-}
+  #app {
+    /* Apply styles for mobile screens */
+  }
 }
 </style>
+
