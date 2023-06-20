67f3ec982875e2eaa42035ebfdd7a276eae46e45
Sussabr <134245813+Sussabr@users.noreply.github.com>
2023-06-20 01:51:59 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -227,6 +227,7 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+   min-height: 100vh;
    position: relative;
   height: 100vh;
   overflow-x: hidden;
@@ -237,6 +238,11 @@ export default {
   box-sizing: border-box;
   margin: 0 auto;
   display: flex;
+   flex: 1; /* Allow the content container to grow and fill the available space */
+  max-width: 1000px; /* Set a maximum width for the content container */
+  width: 100%; /* Set the initial width to 100% */
+  padding: 20px;
+  box-sizing: border-box;
   flex-direction: column;
   }
   #missions,
