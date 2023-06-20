b4fc8a27a4097912673cf056c8bd5c8d0751e952
Sussabr <134245813+Sussabr@users.noreply.github.com>
2023-06-19 23:18:55 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,79 @@
 <template>
   <Header :header="this.header" />
+  <div class="content-container">
+    <section class="section-container" id="missions" style="width:435px; height:714px;">
+      <div class="section-header clipped-medium-backward">
+        <img src="/icons/mission-icon.svg" />
+        <h1>Missões</h1>
+      </div>
+      <div class="section-content-container">
+        <h3>Objetivo Atual</h3>
+        <Markdown :source="current_md" class="markdown" />
+        <h3>Lista de Missões</h3>
+        <div class="mission-list-container">
+          <Mission
+            v-for="item in this.missions"
+            :key="item.slug"
+            :mission="item"
+            :selected="this.mission_slug"
+            @click="selectMission(item)"
+          />
+        </div>
+      </div>
+    </section>
+    <section class="section-container" id="events" style="width:435px; height:714px;">
+      <div class="section-header clipped-medium-backward">
+        <img src="/icons/events-icon.svg" />
+        <h1>Eventos da missão</h1>
+      </div>
+      <div class="section-content-container">
+        <Markdown :source="events" class="markdown" />
+      </div>
+    </section>
+    <section class="section-container" id="pilots" style="width:894px; height:714px;">
+      <div style="height:52px; overflow:hidden;">
+        <div class="section-header clipped-medium-backward-pilot">
+          <img src="/icons/pilot-icon.svg" />
+          <h1>Pilotos</h1>
+        </div>
+        <div class="rhombus-back">&nbsp;</div>
+      </div>
+      <div class="section-content-container">
+        <div class="pilot-list-container">
+          <Pilot v-for="item in this.pilots" :key="item.slug" :pilot="item" />
+        </div>
+      </div>
+    </section>
+  </div>
+  <svg
+    style="visibility: hidden; position: absolute;"
+    width="0"
+    height="0"
+    xmlns="http://www.w3.org/2000/svg"
+    version="1.1"
+  >
+    <defs>
+      <filter id="round">
+        <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
+        <feColorMatrix
+          in="blur"
+          mode="matrix"
+          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -5"
+          result="goo"
+        />
+        <feComposite in="SourceGraphic" in2="goo" operator="atop" />
+      </filter>
+    </defs>
+  </svg>
+  <audio autoplay>
+    <source src="/public/startup.ogg" type="audio/ogg" />
+  </audio>
+  <Footer/>
+</template>
+
+@media (max-width: 767px) { 
+<template>
+  <Header :header="this.header" />
   <v-container>
     <div class="content-container">
       <v-row>
@@ -76,7 +150,7 @@
   </v-container>
   <Footer />
 </template>
-
+} 
 <script>
 import Header from './components/layout/Header.vue';
 import Footer from './components/layout/Footer.vue';
