b9f7a1f1cc5108e64d3e7b60d429558f4f62c9ba
Sussabr <134245813+Sussabr@users.noreply.github.com>
2023-06-19 23:41:38 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,44 @@
 <template>
-  <Header :header="this.header" />
-  <v-container>
-    <div class="content-container">
-      <v-row>
-        <v-col cols="12" md="6">
-          <section class="section-container" id="missions" style="width:435px; height:714px;">
-            <div class="section-header clipped-medium-backward">
-        <img src="/icons/mission-icon.svg" />
-        <h1>Missões</h1>
+  <div id="app">
+    <Header :header="header" />
+    <v-container>
+      <div class="content-container">
+        <v-row>
+          <v-col cols="12">
+            <section class="section-container" id="missions" :style="missionSectionStyle">
+              <!-- Mission section content -->
+            </section>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <section class="section-container" id="events" :style="eventsSectionStyle">
+              <!-- Events section content -->
+            </section>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12">
+            <section class="section-container" id="pilots" :style="pilotsSectionStyle">
+              <!-- Pilots section content -->
+            </section>
+          </v-col>
+        </v-row>
       </div>
-      <div class="section-content-container">
-        <h3>Objetivo Atual</h3>
-        <Markdown :source="current_md" class="markdown" />
-        <h3>Lista de Missões</h3>
-        <div class="mission-list-container">
-          <Mission
-            v-for="item in this.missions"
-            :key="item.slug"
-            :mission="item"
-            :selected="this.mission_slug"
-            @click="selectMission(item)"
-          />
-        </div>
-      </div>
-          </section>
-        </v-col>
-        <v-col cols="12" md="6">
-          <section class="section-container" id="events" style="width:435px; height:714px;">
-             <div class="section-header clipped-medium-backward">
-        <img src="/icons/events-icon.svg" />
-        <h1>Eventos da missão</h1>
-      </div>
-      <div class="section-content-container">
-        <Markdown :source="events" class="markdown" />
-      </div>
-          </section>
-        </v-col>
-      </v-row>
-      <v-row>
-        <v-col cols="12">
-          <section class="section-container" id="pilots" style="width:894px; height:714px ">
-             <div style="height:52px; overflow:hidden;">
-        <div class="section-header clipped-medium-backward-pilot">
-          <img src="/icons/pilot-icon.svg" />
-          <h1>Pilotos</h1>
-        </div>
-        <div class="rhombus-back">&nbsp;</div>
-      </div>
-      <div class="section-content-container">
-        <div class="pilot-list-container">
-          <Pilot v-for="item in this.pilots" :key="item.slug" :pilot="item" />
-        </div>
-      </div>
-          </section>
-        </v-col>
-      </v-row>
-    </div>
-    <svg style="visibility: hidden; position: relative;" width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
-    <defs>
-      <filter id="round">
-        <feGaussianBlur in="SourceGraphic" stdDeviation="5" result="blur" />
-        <feColorMatrix
-          in="blur"
-          mode="matrix"
-          values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -5"
-          result="goo"
-        />
-        <feComposite in="SourceGraphic" in2="goo" operator="atop" />
-      </filter>
-    </defs>
-    </svg>
-    <audio autoplay>
-      <source src="/startup.ogg" type="audio/ogg" />
-    </audio>
-  </v-container>
-  <Footer />
+      <svg style="visibility: hidden; position: relative;" width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="round">
+            <!-- Filter definition -->
+          </filter>
+        </defs>
+      </svg>
+      <audio autoplay>
+        <source src="/startup.ogg" type="audio/ogg" />
+      </audio>
+    </v-container>
+    <Footer />
+  </div>
 </template>
-
 
 <script>
 import Header from './components/layout/Header.vue';
