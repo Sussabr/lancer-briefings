13f7b7d9f99340383277e00f1224c5927554a20a
Sussabr <134245813+Sussabr@users.noreply.github.com>
2023-06-21 01:07:17 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -307,29 +307,5 @@ export default {
     flex-basis: 100%;
     order: 3;
   }
-  .pilot-modal {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  padding: 20px;
-  background-color: rgba(0, 0, 0, 0.8);
-  color: white;
-  z-index: 9999;
-}
-
-.pilot-modal.portrait .pilot {
-  max-width: 40vw;
-  max-height: 20vh;
-}
-  .portrait {
-  max-width: 100%;
-  max-height: 100%;
-  }
   }
 </style>
