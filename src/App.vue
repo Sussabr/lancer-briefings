1f30b09bb7ef2e3d4f5d60d02465d5b8a5c574e0
Sussabr <134245813+Sussabr@users.noreply.github.com>
2023-06-21 00:37:00 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -95,7 +95,12 @@ export default {
       "missions": [
         {
   "slug": "001",
-          "name": "TBA",
+          "name": "Bug Hunt",
+          "status": "start"
+        }, 
+        {
+        "slug": "002",
+          "name": "Vigilant Gaze",
           "status": "start"
         }, 
       ],
