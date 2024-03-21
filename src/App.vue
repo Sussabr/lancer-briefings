3b70b8e4ad0410ea2981c7a33574f0027ccd405c
Sussabr <134245813+Sussabr@users.noreply.github.com>
2024-03-20 23:33:54 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@
               <div style="height:52px; overflow:hidden;">
                 <div class="section-header clipped-medium-backward-pilot">
                   <img src="/icons/pilot-icon.svg" />
-                  <h1>Pilotos</h1>
+                  <h1>Procurados</h1>
                 </div>
                 <div class="rhombus-back">&nbsp;</div>
               </div>
