e50ecb8248fcb1dbe49f758ca91d12a21a83e6b6
Sussabr <134245813+Sussabr@users.noreply.github.com>
2025-06-09 13:58:17 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -124,8 +124,6 @@ export default {
           "frame": "Chomolungma",
           "mech": "Phillips"
         },
-          
-        },
           {
          "callsign": "Maggie",
           "alias": "Margareth Moore",
