ecf36d285b5f70de6bbce9b948d1fd6406f9459b
Sussabr <134245813+Sussabr@users.noreply.github.com>
2025-10-31 17:15:57 -0300

    Update Pilot.vue

--- src/components/Pilot.vue
+++ src/components/Pilot.vue
@@ -133,9 +133,9 @@ import ktbData from 'lancer-ktb-data'
 import nrfawData from 'lancer-nrfaw-data'
 import longrimData from 'lancer-longrim-data'
 
-import wallflowerData from '@/assets/LCPs/wallflower-data-2.0.5'
-import legionnaireData from '@/assets/LCPs/legionnaire_v1.0.0'
-import osrData from '@/assets/LCPs/osr-data-1.2.0'
+import * as wallflowerData from '@/assets/LCPs/wallflower-data-2.0.5'
+import * as legionnaireData from '@/assets/LCPs/legionnaire_v1.0.0'
+import * as osrData from '@/assets/LCPs/osr-data-1.2.0'
 /*Append the datasets within computed if your LCP has new items.
 EX:
 pilotGear() {
