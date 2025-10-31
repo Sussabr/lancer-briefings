71e8ed364d2b616fb9bdccab3571161fcc4f9be9
Sussabr <134245813+Sussabr@users.noreply.github.com>
2025-10-31 16:44:33 -0300

    Update Pilot.vue

--- src/components/Pilot.vue
+++ src/components/Pilot.vue
@@ -135,7 +135,7 @@ import longrimData from 'lancer-longrim-data'
 
 import wallflowerData from '@/assets/LCPs/wallflower-data-2.0.5'
 import legionnaireData from '@/assets/LCPs/legionnaire_v1.0.0'
-import osrData from '@/assets/LCPs/osr-data-1.2.0'
+
 /*Append the datasets within computed if your LCP has new items.
 EX:
 pilotGear() {
