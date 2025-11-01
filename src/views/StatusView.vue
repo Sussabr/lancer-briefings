770d0d1e3f26751b5d119fd86b7a2d90d5744024
Sussabr <134245813+Sussabr@users.noreply.github.com>
2025-11-01 11:23:28 -0300

    Update StatusView.vue

--- src/views/StatusView.vue
+++ src/views/StatusView.vue
@@ -4,7 +4,7 @@
 		<section id="missions" class="section-container" :style="{ 'animation-delay': animationDelay }">
 			<div class="section-header clipped-medium-backward">
 				<img src="/icons/campaign.svg" />
-				<h1>Mission Log</h1>
+				<h1>Log de Missões</h1>
 			</div>
 			<div class="section-content-container">
 				<div class="mission-list-container">
@@ -16,7 +16,7 @@
 		<section id="assignment" class="section-container" :style="{ 'animation-delay': animationDelay }">
 			<div class="section-header clipped-medium-backward">
 				<img src="/icons/deployable.svg" />
-				<h1>Current Assignment</h1>
+				<h1>Missão Atual</h1>
 			</div>
 			<div class="section-content-container">
 				<vue-markdown-it :source="missionMarkdown" class="markdown" />
@@ -37,7 +37,7 @@
 		<section id="clocks" class="section-container" :style="{ 'animation-delay': animationDelay }">
 			<div class="section-header clipped-medium-backward">
 				<img src="/icons/protocol.svg" />
-				<h1>Clocks</h1>
+				<h1>Relógios</h1>
 			</div>
 			<div class="section-content-container">
 				<div class="clocks-list-container">
