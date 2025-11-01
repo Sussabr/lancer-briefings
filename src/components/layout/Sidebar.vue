8c2b3e1135e7d0c45e026f3ad651df9a36502bd9
Sussabr <134245813+Sussabr@users.noreply.github.com>
2025-11-01 12:32:51 -0300

    Update Sidebar.vue

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -15,11 +15,11 @@
 				</router-link>
 				<router-link class="clipped-bottom-right" to="/pilots">
 					<img src="/icons/pilot.svg" />
-					<span>Pilots</span>
+					<span>Pilotos</span>
 				</router-link>
 				<router-link class="clipped-bottom-right" to="/events">
 					<img src="/icons/events.svg" />
-					<span>Logs</span>
+					<span>Registros</span>
 				</router-link>
 			</o-sidebar>
 		</section>
