b81d231e11dc1cad5429953148a62da3a967ee8d
Sussabr <134245813+Sussabr@users.noreply.github.com>
2025-11-05 09:50:32 -0300

    Update Pilot.vue

--- src/components/Pilot.vue
+++ src/components/Pilot.vue
@@ -143,7 +143,7 @@ V-CDL//M-265-114-831(A) </span></div>
 .kia-text {
   font-size: 4rem;
   font-weight: bold;
-  font-family: "Helvetica";
+  font-family: "compcon";
   color: #c60000;
   text-align: center;
   text-transform: uppercase;
