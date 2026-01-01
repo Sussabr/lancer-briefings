9d4b4da3bbf90d15592c7c5104ba906db7554fdb
Sussabr <134245813+Sussabr@users.noreply.github.com>
2026-01-01 13:01:46 -0300

    Fix import path for responsive.css

--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import "@/assets/styles/_reset.css";
 import "@/assets/styles/_base.css";
 import "@/assets/styles/_glyphs.css";
 import "@/assets/styles/_animations.css";
-import "@/assets/responsive.css";
+import "@/assets/styles/responsive.css";
 import router from "./router";
 
 // IMPORTANTE: importar a lib
