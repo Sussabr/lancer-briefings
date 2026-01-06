5eb742c3a0291dc26ec185fcca18d2f2bb0b6d23
Sussabr <134245813+Sussabr@users.noreply.github.com>
2026-01-06 14:57:56 -0300

    Refactor Sidebar component for mobile responsiveness

--- src/components/layout/Sidebar.vue
+++ src/components/layout/Sidebar.vue
@@ -1,47 +1,55 @@
 <template>
-	<div class="sidebar-page">
-		<section class="sidebar-layout">
-			<o-sidebar
-			  id="sidebar"
-			  position="static"
-			  :animate="animate"
-			  :mobile="mobile"
-			  :expand-on-hover="expandOnHover"
-			  :reduce="reduce"
-			  open>
-				<router-link class="clipped-bottom-right" to="/status">
-					<img src="/icons/orbital.svg" />
-					<span>Status</span>
-				</router-link>
-				<router-link class="clipped-bottom-right" to="/pilots">
-					<img src="/icons/pilot.svg" />
-					<span>Pilotos</span>
-				</router-link>
-				<router-link class="clipped-bottom-right" to="/events">
-					<img src="/icons/events.svg" />
-					<span>Registros</span>
-				</router-link>
-			</o-sidebar>
-		</section>
-	</div>
+  <div class="sidebar-page">
+    <section class="sidebar-layout">
+      <o-sidebar
+        id="sidebar"
+        position="static"
+        :animate="animate"
+        mobile="hidden"
+        :open="!isMobile"
+        :expand-on-hover="false"
+        :reduce="false"
+      >
+        <router-link class="clipped-bottom-right" to="/status">
+          <img src="/icons/orbital.svg" />
+          <span>Status</span>
+        </router-link>
+
+        <router-link class="clipped-bottom-right" to="/pilots">
+          <img src="/icons/pilot.svg" />
+          <span>Pilotos</span>
+        </router-link>
+
+        <router-link class="clipped-bottom-right" to="/events">
+          <img src="/icons/events.svg" />
+          <span>Registros</span>
+        </router-link>
+      </o-sidebar>
+    </section>
+  </div>
 </template>
 
 <script>
 export default {
-	props: {
-		animate: {
-			type: Boolean,
-			required: true,
-		},
-	},
-	data() {
-		return {
-			expandOnHover: false,
-			mobile: "reduced",
-			reduce: false,
-		};
-	},
+  props: {
+    animate: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      isMobile: window.innerWidth <= 768,
+    };
+  },
+  mounted() {
+    this.onResize = () => {
+      this.isMobile = window.innerWidth <= 768;
+    };
+    window.addEventListener("resize", this.onResize);
+  },
+  beforeUnmount() {
+    window.removeEventListener("resize", this.onResize);
+  },
 };
 </script>
-
-<!-- <style></style> -->
