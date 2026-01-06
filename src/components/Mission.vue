bb779072e8b5311f696b6a424c1fbf5f8e1010ae
Sussabr <134245813+Sussabr@users.noreply.github.com>
2026-01-06 14:47:10 -0300

    Translate mission status messages to Portuguese

--- src/components/Mission.vue
+++ src/components/Mission.vue
@@ -29,10 +29,10 @@ export default {
       return `/icons/mission-${this.mission.status}.svg`;
     },
     missionStatus() {
-      if (this.mission.status === "start") return "Current\nBriefing";
-      if (this.mission.status === "partial-success") return "Partial\nSuccess";
-      if (this.mission.status === "success") return "Mission\nSuccess";
-      if (this.mission.status === "failure") return "Mission\nFailure";
+      if (this.mission.status === "start") return "Missão\nAtual";
+      if (this.mission.status === "partial-success") return "Sucesso\nParcial";
+      if (this.mission.status === "success") return "Sucesso\nCompleto";
+      if (this.mission.status === "failure") return "Missão\nFalhada";
     },
     isActive() {
       return this.mission.slug === this.selected;
